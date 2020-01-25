4d7b4d1547deceaac6f9c66bf530fd7032d9b2cb
Van Huy <huyvk3110@gmail.com>
2020-01-25 18:35:25 +0700

    Done

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,51 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+var axios = require('axios');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function (app, db) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(function (req, res) {
+      var { stock, like } = req.query;
+      const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+      const getStock = (stock, isLike, ip) => new Promise((res, rej) => {
+        db.collection('stocks').findOne({ stock })
+          .then(data => {
+            if (data && isLike) {
+              if (!data.likes.find(o => o == ip)) data.likes.push(ip);
+              db.collection('stocks').updateOne({ stock }, data)
+                .then(dat => { res({ stock: data.stock, price: data.price, likes: data.likes.length }) })
+                .catch(error => rej(error))
+            } else if (data && !isLike) {
+              res({ stock: data.stock, price: data.price, likes: data.likes.length })
+            } else {
+              axios.get(`https://repeated-alpaca.glitch.me/v1/stock/${stock.toUpperCase()}/quote`)
+                .then(data => {
+                  const price = data.data.latestPrice;
+                  db.collection('stocks').insertOne({ stock, price, likes: isLike ? [ip] : [] })
+                    .then(data => res({ stock: data.ops[0].stock, price: data.ops[0].price, likes: data.ops[0].likes.length }))
+                    .catch(error => rej(error))
+                })
+                .catch(error => rej(error));
+            }
+          })
+          .catch(error => rej(error))
+      })
+
+      if (typeof stock === 'string') {
+        getStock(stock, like, ip).then(data => res.json({ "stockData": data })).catch(error => res.json({ error }));
+      } else if (typeof stock === 'object' && stock.length) {
+        stock = stock.slice(0, 2);
+        Promise.all(stock.map(o => getStock(o, like, ip))).then(data => {
+          res.json({"stockData":[{"stock":data[0].stock,"price":data[0].price,"rel_likes":data[0].likes - data[1].likes},{"stock":data[1].stock,"price":data[1].price,"rel_likes":data[1].likes - data[0].likes}]})
+        }).catch(error => res.json({ error }));
+      } else {
+        res.json({ error: 'Error' })
+      }
     });
-    
+
 };
--- server.js
+++ server.js
@@ -8,50 +8,62 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+var helmet      = require('helmet');
+var mongo       = require('mongodb').MongoClient;
+var dotenv      = require('dotenv');
 
 var app = express();
+dotenv.config();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
+app.use(helmet.contentSecurityPolicy({directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'"] }}))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+mongo.connect(process.env.DB, function(error, client) {
+  if(error) return console.log(error);
+
+  const db = client.db('freecodecamp');
+
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+
+  //For FCC testing purposes
+  fccTestingRoutes(app);
+
+  //Routing for API 
+  apiRoutes(app,db);  
+      
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
-});
+  //Start our server and tests!
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on port " + process.env.PORT);
+    if(process.env.NODE_ENV==='test') {
+      console.log('Running Tests...');
+      setTimeout(function () {
+        try {
+          runner.run();
+        } catch(e) {
+          var error = e;
+            console.log('Tests are not valid:');
+            console.log(error);
+        }
+      }, 3500);
+    }
+  });
+})
 
 module.exports = app; //for testing
